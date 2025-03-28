be2e09877b73467c11421ecd267bd18a7e7acfb1
Arina Mirzakhani <Arina.Mirzakhani@georgebrown.ca>
2025-03-28 10:07:37 -0400

    Lab week 12

---
+++ puma.py
@@ -0,0 +1,15 @@
+from mammal import Mammal
+
+class Puma(Mammal):
+    def __init__(self, age, tick=None):
+        super().__init__(age)
+
+        if tick:
+            self.tick = tick
+
+    def speak(self):
+        print("Roar")
+
+    def __str__(self):
+        return f"Puma is {self.age} years old"
+
